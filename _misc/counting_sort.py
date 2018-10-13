d1b6a0f71b375f409b9b767b9a21067dcaa06a2f
Jeremías <jrmsdev@gmail.com>
2018-10-13 17:19:53 -0300

    debug counting sort

--- _misc/counting_sort.py
+++ _misc/counting_sort.py
@@ -1,27 +1,39 @@
 #!/usr/bin/env python3
 
-from itertools import accumulate
+# Constraints:
+#   len (arr) <= 9
+#   1 <= arr[i] <= 9
 
 arr = [7, 6, 4, 3, 2, 3]
-print (arr)
+print ('Orig array:  ', arr)
 
+# init counter
 count = [0 for _ in range (9)]
-print ('C0:', count)
+print ('Init counter:', count)
 
+# count frequency
 for n in arr:
-    count[n] += 1
-print ('C1:', count)
+    count[n] += 1 # +=1 to allow duplicates
+    print ('Count val:   ', n, '->', count[n])
+print ('Count:       ', count)
 
-count = list (accumulate (count))
-print ('C2:', count)
+# count frequency accumulate
+for i in range (9):
+    count[i] += count[i - 1]
+    print ('Accumulate:  ', i, '->', count[i])
+print ('Accum:       ', count)
 
-outarr = [None for _ in range (9)]
-print ('O0:', outarr)
+# init output array
+outarr = [0 for _ in range (9)]
+print ('Init output: ', outarr)
 
-for n in arr:
-    outarr[count[n]] = n
-    count[n] -= 1 # allow duplicates
-print ('O1:', outarr)
+# populate output array
+for i in range (len (arr)):
+    x = count[arr[i]] - 1
+    outarr[x] = arr[i]
+    print ('Output:      ', 'arr', i, 'pos', x, '->', arr[i])
+    count[arr[i]] -= 1 # allow duplicates
+print ('Output:      ', outarr)
 
-arr = [n for n in outarr if n is not None]
-print (arr)
+arr = [n for n in outarr if n > 0]
+print ('Sorted array:', arr)
